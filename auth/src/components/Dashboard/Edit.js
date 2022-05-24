58f218fd383e30b0fb4f8aab4b17faf196835497
Hitesh Zala <hitesh.fluxbyte@gmail.com>
2022-05-24 18:41:52 +0530

    clean

--- auth/src/components/Dashboard/Edit.js
+++ auth/src/components/Dashboard/Edit.js
@@ -1,7 +1,7 @@
 
 import React, { useState, useEffect } from "react";
-import axios from "axios";
-import { useNavigate, useParams } from "react-router-dom";
+// import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const EditUser = () => {
   let navigate = useNavigate();
