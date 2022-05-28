5775297631ebd684f885d66494c8011b0e6a4637
Hitesh Zala <hitesh.fluxbyte@gmail.com>
2022-05-28 13:10:31 +0530

    NewsItem Converts Class Based to Fucntion Based

--- my-auth/src/Components/news/NewsItem.js
+++ my-auth/src/Components/news/NewsItem.js
@@ -1,48 +1,93 @@
-import React, { Component } from "react";
+import React from "react";
 
-class NewsItem extends Component {
-  render() {
-    let { title, description, imageUrl, newsUrl } = this.props;
-    return (
-      <>
-        <div className="my-3">
-          <div className="card " style={{ height: "430px" }}>
-            <img
-              src={imageUrl}
-              className="card-img-top"
-              alt="..."
-              style={{ height: "35%" }}
-            />
-            <div
-              className="card-body d-flex flex-column"
-              style={{ height: "65%" }}
+
+// Function Based Components
+function NewsItem(props) {
+  let { title, description, imageUrl, newsUrl } = props;
+  return (
+    <>
+      <div className="my-3">
+        <div className="card " style={{ height: "430px" }}>
+          <img
+            src={imageUrl}
+            className="card-img-top"
+            alt="..."
+            style={{ height: "35%" }}
+          />
+          <div
+            className="card-body d-flex flex-column"
+            style={{ height: "65%" }}
+          >
+            <h5
+              className="titleLine4-ellipse card-title overflow-hidden"
+              style={{ height: "40%" }}
             >
-              <h5
-                className="titleLine4-ellipse card-title overflow-hidden"
-                style={{ height: "40%" }}
-              >
-                {title}{title}
-              </h5>
-              <p
-                className="card-text overflow-hidden"
-                style={{ height: "40%" }}
-              >
-                {description}
-              </p>
-              <a
-                href={newsUrl}
-                target="_blank"
-                className="btn btn-sm btn-dark"
-                rel="noreferrer"
-              >
-                Read More
-              </a>
-            </div>
+              {title}
+              {title}
+            </h5>
+            <p className="card-text overflow-hidden" style={{ height: "40%" }}>
+              {description}
+            </p>
+            <a
+              href={newsUrl}
+              target="_blank"
+              className="btn btn-sm btn-dark"
+              rel="noreferrer"
+            >
+              Read More
+            </a>
           </div>
         </div>
-      </>
-    );
-  }
+      </div>
+    </>
+  );
 }
 
 export default NewsItem;
+
+
+// Class Based Components
+// class NewsItem  extends Component = (props) => {
+//     let { title, description, imageUrl, newsUrl } = props;
+//     return (
+//       <>
+//         <div className="my-3">
+//           <div className="card " style={{ height: "430px" }}>
+//             <img
+//               src={imageUrl}
+//               className="card-img-top"
+//               alt="..."
+//               style={{ height: "35%" }}
+//             />
+//             <div
+//               className="card-body d-flex flex-column"
+//               style={{ height: "65%" }}
+//             >
+//               <h5
+//                 className="titleLine4-ellipse card-title overflow-hidden"
+//                 style={{ height: "40%" }}
+//               >
+//                 {title}{title}
+//               </h5>
+//               <p
+//                 className="card-text overflow-hidden"
+//                 style={{ height: "40%" }}
+//               >
+//                 {description}
+//               </p>
+//               <a
+//                 href={newsUrl}
+//                 target="_blank"
+//                 className="btn btn-sm btn-dark"
+//                 rel="noreferrer"
+//               >
+//                 Read More
+//               </a>
+//             </div>
+//           </div>
+//         </div>
+//       </>
+//     );
+//   }
+
+// export default NewsItem;
